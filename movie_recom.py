82c2cf9b24ed0609aa6f9693640af41ec2ab4593
PurnanandWasave <46159709+PurnanandWasave@users.noreply.github.com>
2019-01-10 12:57:09 +0530

    Update movie_recom.py

--- movie_recom.py
+++ movie_recom.py
@@ -27,6 +27,48 @@ def get_recommendations(dataset,input_user):
     similarity_score = pearson_score(dataset,input_user,user)
     if similarity_score <= 0:
       continue
+  
+#Extract list of movies
+  filtered_list = [x for x in dataset[user] if x not in \ dataset[input_user] or dataset[input_user][x] == 0]
+      
+#Rating of movies
+    for item in filtered_list:
+      overall_scores.update({item: dataset[user][item] * similarity_score})
+      similarity_scores.update({item: similarity_score})
+      
+  if len(overall_scores) == 0:
+    return['No recommendation possible']
+
+  movie_scores = np.array([score/similarity_scores[item], item] for item, score in overall_scores.items()])
+  
+#Sort movie recommendations
+  movie_scores = movie_scores[np.argsort(movie_scores[:, 0])[: :-1]]
+  movie_recommendations = [movie for _, movie in movie_scores]
+  
+  return movie_recommendations
+
+#Extract the name of input user
+if _name_=='_main_':
+  args = build_arg_parser().parse_args()
+  user = args.user
+  
+#Load rating from movie_ratings.json
+  ratings_file = 'movie_ratings.json'
+  with open(ratingss_file,'r') as f:
+    data = json.loads(f.read())
+    
+  print("\nMovie Recommendations for "+ user +":")
+  movies = get_recommendations(data, user)
+  for i, movie in enumerate(movies):
+    print(str(i+1) + '.' + movie)
+    
+##The Movie Recommendation is ready ##
+    
+
+  
+  
+      
+      
       
       
       
